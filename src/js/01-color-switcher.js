5277a9ade12ea6df124c168cd433d72495a3b48e
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-01-29 23:22:56 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,7 @@ const body = document.body;
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 
-timerId = null;
+let timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
