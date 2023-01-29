860797e8ce29f6c33f8e8e705eabbee32fb2fc54
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-01-29 22:37:56 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+
 function createPromise(position, delay) {
+
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  return new Promise((resolve, reject) => {   
+    
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay);
+  });  
+};
+
+form.addEventListener('submit', onsubmitForm);
+
+function onsubmitForm(event) {
+  event.preventDefault();  
+
+  const amount = Number(event.target.elements.amount.value);
+  const step = Number(event.target.elements.step.value);
+  let firstDelay = Number(event.target.elements.delay.value);  
+  
+  for (let i = 1; i <= amount; i += 1){
+    
+    createPromise(i, firstDelay)
+    .then(({position, delay }) => Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+    .catch(({position, delay}) => Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+    
+    firstDelay += step;    
+  };
+  
+  form.reset();
+};
+
+
